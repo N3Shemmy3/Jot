f33e5478b82884e44a428fa612d30e78393f257b
Shemmy <shemmynyirenda3@gmail.com>
2025-03-31 21:55:39 +0200

    added components

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dark w-full h-dvh bg-background text-muted-foreground">
+    <header class="fixed left-0 top-0 right-0 h-14 shadow-md"></header>
+    <main class="pt-14">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h4>index.vue</h4>
+</template>
