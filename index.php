4ec000e48079d43ce607d11c43e23d31c50fa2f1
Shemmy <shemmynyirenda3@gmail.com>
2025-04-04 14:38:37 +0200

    implement user authentication; add signup and login functionality with database integration

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include_once('./src/Auth.php');
+include_once('./src/Responses.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $action = $_POST['action'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    switch ($action) {
+        case 'signup':
+            signup($username, $password);
+            break;
+        case 'login':
+            login($username, $password);
+            break;
+        default:
+            echo Responses::error("Invalid action.");
+            break;
+    }
+} else {
+
+    if ($conn->connect_error) {
+        echo Responses::error("Connection failed: " . $conn->connect_error);
+    } else {
+        echo Responses::success("Welcome to the Jot api :D");
+    }
+}
